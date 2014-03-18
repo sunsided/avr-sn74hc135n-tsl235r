0c516280410398a08350f0f49e37af1d9dca9c2c
Markus Mayer <development@widemeadows.de>
2014-03-18 01:40:35 +0100

    added timer1

--- light-measure/light-measure.c
+++ light-measure/light-measure.c
@@ -18,18 +18,34 @@
 void external_timer0_init()
 {
 	DDRD  &= ~(1 << DDD4);      /* T0 pin as input */
-	PORTD |= (1 << PORTD4);     /* pull-up enabled */
+	PORTD  |= (1 << PORTD4);    /* pull-up enabled */
 	TIMSK0 |= (1 << TOIE0);     /* enable timer0 interrupt */
 	TCCR0B |= (1 << CS02)       /* enabled counter, count on rising edge */
 	       |  (1 << CS01) 
 	       |  (1 << CS00);
 }
 
-volatile uint8_t timer0_ofs = 0;
+void external_timer1_init()
+{
+	DDRD  &= ~(1 << DDD5);      /* T1 pin as input */
+	PORTD  |= (1 << PORTD5);    /* pull-up enabled */
+	TIMSK1 |= (1 << TOIE1);     /* enable timer1 interrupt */
+	TCCR1B |= (1 << CS12)       /* enabled counter, count on rising edge */
+	       |  (1 << CS11)
+	       |  (1 << CS10);
+}
+
+volatile uint_fast16_t timer0_ofs = 0;
+volatile uint_fast16_t timer1_ofs = 0;
 
 ISR (TIMER0_OVF_vect)
 {
 	++timer0_ofs;
+}
+
+ISR (TIMER1_OVF_vect)
+{
+	++timer1_ofs;
 }
 
 int main(void)
@@ -44,6 +60,7 @@ int main(void)
 
 	/* initialize external timers */
 	external_timer0_init();
+	external_timer1_init();
 	
 	/* enable interrupts */
 	sei();
